fd88e3280942e7e06c8c7fa7a1a75a0cb4c176c6
italoviola <italoviola46@gmail.com>
2025-03-19 17:50:40 -0300

    Adjusts at ContourForm to use useRelatedTools hook

--- src/components/ContourForm/index.tsx
+++ src/components/ContourForm/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { ContourType, Machining, Contours } from 'types/part';
 
@@ -14,12 +14,12 @@ import {
 import useFormattedTools from 'hooks/useFormattedTools';
 import { addContour, editContour } from 'state/part/partSlice';
 
-import useFormattedDressingTools from 'hooks/useFormattedDressingTools';
+import useRelatedTools from 'hooks/useRelatedTools';
 
 import toolNames from 'mockdata/pt-br/dressingTools.json';
 
 import { FieldState } from 'components/FormField/interface';
-import { ToolOptionItem, ToolOptions } from 'components/Select/interface';
+import { ToolOptionItem } from 'components/Select/interface';
 import { addContourPayload, FormProps, IFormData } from './interface';
 
 import {
@@ -48,7 +48,7 @@ const ContourForm: React.FC<FormProps> = ({
 }) => {
   const dispatch = useDispatch();
   const formattedTools = useFormattedTools();
-  const fDressingTools = useFormattedDressingTools();
+  const relatedTools = useRelatedTools();
   const availableTypes = Array.from(
     new Set(formattedTools.map((tool: ToolOptionItem) => tool.type)),
   );
@@ -80,15 +80,6 @@ const ContourForm: React.FC<FormProps> = ({
   }
 
   const [formData, setFormData] = useState(formValues);
-  const [matchedTools, setMatchedTools] = useState<ToolOptions>([]);
-
-  useEffect(() => {
-    setMatchedTools(
-      formattedTools.filter(
-        (tool) => tool.type === Number(formData.type.value),
-      ),
-    );
-  }, [formData.type.value, formattedTools]);
 
   const handleChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>,
@@ -246,55 +237,44 @@ const ContourForm: React.FC<FormProps> = ({
               <Message>{formData.dressingTool.message}</Message>
             )}
             <TitleLabel>Ferramenta de Dressagem</TitleLabel>
-            <Field>
-              {['tool1', 'tool2', 'tool3', 'tool4'].map((toolPrefix, index) => {
-                const toolsForPrefix = fDressingTools.filter((tool) =>
-                  tool.name.startsWith(toolPrefix),
-                );
-                if (toolsForPrefix.length === 0) return null;
-
-                const matchedToolsForPrefix = toolsForPrefix.filter((tool) =>
-                  matchedTools.some(
-                    (matchedTool) => matchedTool.id === tool.toolId,
-                  ),
-                );
-                if (matchedToolsForPrefix.length === 0) return null;
-
+            {Object.entries(relatedTools).map(
+              ([toolKey, dToolNames], index) => {
+                const toolId = parseInt(toolKey.replace('tool', ''), 10);
+                const toolType = formattedTools.find(
+                  (tool) => tool.id === toolId,
+                )?.type;
+                if (toolType !== Number(formData.type.value)) {
+                  return null;
+                }
                 return (
-                  <div key={toolPrefix}>
+                  <div key={toolKey}>
                     <Field>
                       <Label>Rebolo {index + 1}</Label>
                     </Field>
-                    {matchedToolsForPrefix.map((tool) => {
-                      const noPrefixToolName = tool.name
-                        .replace(/tool[1-4]/, '')
-                        .replace('Qtd', '');
+                    {dToolNames.map((name) => {
+                      const toolName = name.replace(/\d+$/, '');
                       const translatedToolName =
-                        toolNames[noPrefixToolName as keyof typeof toolNames];
-
+                        toolNames[toolName as keyof typeof toolNames];
+                      const toolNumber = name.match(/\d+$/);
                       return (
-                        <Field key={tool.name}>
-                          {[...Array(tool.quantity)].map((_, i) => (
-                            <RadioButton style={{ fontSize: '16px' }}>
-                              <input
-                                type="radio"
-                                value={`${noPrefixToolName.replace('Qtd', '')}${
-                                  i + 1
-                                }`}
-                                name="dressingTool"
-                                onChange={(e) => handleChange(e)}
-                              />
-                              <span />
-                              {`${translatedToolName} ${i + 1}`}
-                            </RadioButton>
-                          ))}
+                        <Field key={`${toolKey}-${name}`}>
+                          <RadioButton style={{ fontSize: '16px' }}>
+                            <input
+                              type="radio"
+                              value={name}
+                              name="dressingTool"
+                              onChange={(e) => handleChange(e)}
+                            />
+                            <span />
+                            {`${translatedToolName} ${toolNumber}`}
+                          </RadioButton>
                         </Field>
                       );
                     })}
                   </div>
                 );
-              })}
-            </Field>
+              },
+            )}
           </>
         )}
       {machining === MACHINING_DRESSING && (
